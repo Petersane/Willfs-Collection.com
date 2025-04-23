f58f974b31fd10b0e761329978f211455c5b330c
Petersane <sanetipeter34@gmail.com>
2025-04-23 11:13:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuicon=document.getElementById("menuicon");
+const menuList=document.getElementById("menuList");
+
+menuicon.addEventListener("click",()=>{
+    menuList.style.display="block";
+})
